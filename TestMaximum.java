1cbc2ce05481883c03972611e63552add8a4f723
omkar <omkardudhewar121@gmail.com>
2022-12-08 01:03:55 +0530

    added: maximum at 3rd position

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,15 +1,15 @@
 package CoreJava;
 
 public class TestMaximum {
-    public static Integer compareTo(Integer a,Integer b,Integer c) {
-        if(a > b){
-            if(a > c) {
+    public static Float compareTo(Float a, Float b, Float c) {
+        if (a > b) {
+            if (a > c) {
                 return a;
             } else {
                 return c;
             }
         } else {
-            if(b > c) {
+            if (b > c) {
                 return b;
             } else {
                 return c;
@@ -19,7 +19,7 @@ public class TestMaximum {
 
     public static void main(String[] args) {
         System.out.println("----!Welcome to Generic Test Maximum!-----");
-        int max=compareTo(10,10,900);
-        System.out.println("Maximum from 3 Integer : "+max);
+        Float maxFloat = compareTo(10.50F, 10.70F, 90.60F);
+        System.out.println("Maximum from 3 Float : " + maxFloat);
     }
 }
