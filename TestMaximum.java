bb0543ed3e16ea59d8ad3cd88c1ed02d345945c1
omkar <omkardudhewar121@gmail.com>
2022-12-07 13:39:28 +0530

    added:test case to find maximum integer among 3

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,8 +1,25 @@
 package CoreJava;
 
 public class TestMaximum {
+    /*
+    To test which integer is maximum using java generics
+     */
+    public static Integer compareTo(Integer a, Integer b, Integer c) {
+        /*
+        comparing 3 Integers by if-else condition
+        return = Maximum Integer
+         */
+        if (a > b && a > c) {
+            return a;
+        }
+        else if (b > a && b > c) {
+            return b;
+        }
+        else return c;
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome To Generics Test Maximum");
+        int testMaximum = compareTo(30,70,50);
+        System.out.println("Maximum Integer among 3 = "+testMaximum);
 
     }
 }
